5ee4785fa0f4f48502245b98a5615c4b37ac1a0b
Mikołaj Borecki <mikolaj.borecki1@gmail.com>
2022-01-23 17:02:10 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -18,10 +18,13 @@ message.set_content(body)
 
 context = ssl.create_default_context() #we need a secure connection while connecting to Gmail
 
+print("Sending Email!")
 
-
-
-
-
-
+#port 465 - secure e-mail sending with SSL - this port is no longer used
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+  server.login(sender_email, password) #logging to server
+  server.sendemail(sender_email, receiver_email, message.as string)
+  
+print("Success")
+  
 #possible improvements - sending files, sending to multiple people
